c372361c8c633e2f98911ba2d285304df07227f9
Roshan Sharma <reprogram46@gmail.com>
2020-04-27 06:45:19 +0530

    client: Add non-functional uploader in Index.vue
    Regular placeholder stuff.

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -1,54 +1,22 @@
 <template>
   <q-page class="flex flex-center">
-    <div>
-      hi
-      <chart :series="series" :options="chartOptions" type="line"></chart>
+    <div class="q-mx-lg">
+      <p class="text-subtitle1 text-center">
+        To get started, upload an Excel spreadsheet file.
+      </p>
+      <p class="text-center">
+        <!-- <q-btn
+          glossy
+          icon-right="file_upload"
+          label="Upload"
+          class="q-ma-lg"
+        /> -->
+        <q-uploader
+          url="http://localhost:4444/upload"
+          dark
+          color="green"
+        />
+      </p>
     </div>
   </q-page>
 </template>
-
-<script>
-import VueApexChart from 'vue-apexcharts'
-
-export default {
-  name: 'PageIndex',
-
-  components: {
-    Chart: VueApexChart
-  },
-
-  data() {
-    return {
-      series: [{
-        name: 'Top Score',
-        data: [50, 40, 45, 50, 49, 60, 70, 19]
-      }],
-      chartOptions: {
-        chart: {
-          type: 'line',
-          height: 'auto',
-          zoom: {
-            enabled: false
-          }
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-        },
-        grid: {
-          row: {
-            borderColor: ['#f00', '#00f'],
-            opacity: 0.8,
-            show: true,
-            strokeDashArray: 0,
-            xaxis: {
-              lines: {
-                show: true
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-</script>
