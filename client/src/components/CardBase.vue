522fc16905872d2c9838259e2c25fe8480a769b0
Roshan Sharma <reprogram46@gmail.com>
2020-04-27 06:46:17 +0530

    client/feat: Create Graph component (deps CardBase)
    Reusable graph component, color configurable.

---
+++ client/src/components/CardBase.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-card class="fit" :style="{backgroundColor: bgColor}"  >
+    <q-card-section>
+      <slot></slot>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'CardBase',
+  props: {
+    bgColor: {
+      default: '#2B2D3E5'
+    }
+  }
+}
+</script>
---
+++ client/src/components/Graph.vue
@@ -0,0 +1,36 @@
+<template>
+  <card-base>
+     <apex-chart height="300px" width="300px" type="scatter" :options="chartOptions" :series="series" />
+  </card-base>
+</template>
+
+<script>
+import CardBase from 'components/CardBase'
+import VueApexChart from 'vue-apexcharts'
+
+export default {
+  components: {
+    CardBase,
+    ApexChart: VueApexChart
+  },
+
+  data() {
+    return {
+      series: [{
+        name: 'Item',
+        data: [3,4,6,7,4,2,5,7,8,]
+      }],
+      chartOptions: {
+        xaxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+          labels: {
+            style: {
+              colors: '#fff'
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</script>
