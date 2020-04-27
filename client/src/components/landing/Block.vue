656b581d03c86b3feec05ece03d638707a693eea
Roshan Sharma <reprogram46@gmail.com>
2020-04-28 01:47:10 +0530

    client: Add landing page skeleton with slot
    TBD: Choose nice color theme and text.

---
+++ client/src/components/landing/Block.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="q-my-lg q-py-xs text-center"
+    :style="'backgroundColor: ' + bg + '; color: ' + font"
+  >
+    <slot>Get Started!</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    bg: {
+      default: '#006'
+    },
+    font: {
+      default: '#999'
+    }
+  }
+}
+</script>
--- client/src/layouts/HomeLayout.vue
+++ client/src/layouts/HomeLayout.vue
@@ -1,21 +1,39 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar class="bg-custom-blue-dark">
-        <q-toolbar-title>
-          Trendlyzer!
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-header>
+    <main class="non-selectable">
+      <!-- The primary block -->
+      <div class="q-pa-lg">
+        <p class="q-py-xl text-h2 text-weight-medium">
+          Visual insights from your data
+        </p>
+        <p class="text-h4">
+          Say hello to data analytics.
+        </p>
+      </div>
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
+      <!-- Subsequent blocks -->
+      <block>
+        <h4>Get started!</h4>
+      </block>
+    </main>
   </q-layout>
 </template>
 
 <script>
+import Block from 'components/landing/Block'
+
 export default {
-  name: 'HomeLayout'
+  name: 'HomeLayout',
+
+  components: {
+    Block
+  }
 }
 </script>
+
+<style scoped>
+main div {
+  background-color: #008;
+  color: #fff;
+}
+</style>
