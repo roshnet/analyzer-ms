51279cb4aeac758b254f3b2b18c584ef8fe1452e
Roshan Sharma <reprogram46@gmail.com>
2020-04-27 06:47:49 +0530

    client/feat: Configure chart interface; use dummy graphs
    Set the UI up. The app can now consume the values from the API and display.

--- client/src/pages/Charts.vue
+++ client/src/pages/Charts.vue
@@ -1,44 +1,45 @@
 <template>
   <q-page class="flex flex-center">
-    <div>
-      charts
-      <chart :series="series" :options="chartOptions" height="400" width="500"></chart>
+    <div class="row q-col-gutter-md">
+      <div class="col-md-6 col-sm-12 col-xs-12">
+        <graph></graph>
+      </div>
+      <div class="col-md-6 col-sm-12 col-xs-12">
+        <graph></graph> 
+      </div>
+      <div class="col-md-6 col-sm-12 col-xs-12">
+        <graph></graph>
+      </div>
+      <div class="col-md-6 col-sm-12 col-xs-12">
+        <graph bgColor="red"></graph>
+      </div>
+      <div class="col-md-6 col-sm-12 col-xs-12">
+        <graph></graph>
+      </div>
+      <div class="col-md-6 col-sm-12 col-xs-12">
+        <graph></graph>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import VueApexChart from 'vue-apexcharts'
+import Graph from 'components/Graph'
 
 export default {
-  name: 'PageCharts',
-  
+  name: 'ChartsPage',
+
   components: {
-    Chart: VueApexChart
+    Graph
   },
 
   data() {
     return {
-      series: [
-        {
-          // the info for the second plot
-          name: "Smartphones",
-          data: [23, 34, 45, 56, 67, 78]
-        },
-        {
-          // the info for the first plot
-          name: "Televisions",
-          data: [28, 29, 33, 36, 32, 32, 33]
-        },
-      ],
+      fetchedContent: [],
+      series: [],
       chartOptions: {
-        chart: {
-          type: 'bar',
-          height: 400
-        },
-        colors: ['#080', '#008'],
         xaxis: {
-           categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+           categories: []
         }
       }
     }
