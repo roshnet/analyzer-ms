57e27c98bcb7158dd128311ee5d49142f6757688
Roshan Sharma <reprogram46@gmail.com>
2020-05-05 05:43:55 +0530

    client/wip: Create component 'Compare' for pivot analysis
    This state is WIP. Need to decide how to proceed to viewing graphs based on fetched data.

---
+++ client/src/components/Compare.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <q-btn-dropdown :disable="!columnsReady" color="primary" label="Available Columns">
+      <q-list>
+        <q-item
+          v-for="(column, i) in columns"
+          :key="i"
+          @click="getPivotValues(column)"
+          clickable
+          v-close-popup
+        >
+          <q-item-section>
+            <q-item-label>{{ column }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // hold names of all columns as an array
+      columns: [],
+
+      // do not activate the dropdown until the columns are done fetching
+      columnsReady: false,
+    }
+  },
+
+  methods: {
+    getColumnNames() {
+      const endpoint = process.env.DEV_API + '/summary'
+      this.$axios
+        .get(endpoint)
+        .then((response) => {
+          this.columns = Object.keys(response.data.result)
+          this.columnsReady = true  // enables the columns dropdown
+        })
+        .catch((err) => {
+          console.error(">>> Shit hit the fan: ", err)
+        })
+    },
+
+    getPivotValues(column) {
+      const endpoint = process.env.DEV_API + '/pivot?about=' + column
+      this.$axios
+        .get(endpoint)
+        .then((response) => {
+          console.warn(response.data.result);
+        })
+        .catch((err) => {
+          console.error(">>> Shit hit the fan: ", err)
+        })
+    }
+  },
+
+  mounted() {
+    this.getColumnNames()
+  }
+}
+</script>
