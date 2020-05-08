0571546a9737ce759943fd1bb8a699455304c91e
Roshan Sharma <reprogram46@gmail.com>
2020-05-08 06:27:45 +0530

    client/wip: Prepare Compare to plot analyzer graphs on click
    Set up show/hide category; enhance analyzer UI.

--- client/src/components/Compare.vue
+++ client/src/components/Compare.vue
@@ -1,25 +1,45 @@
 <template>
   <div>
-    <q-btn-dropdown :disable="!columnsReady" color="primary" label="Available Columns">
-      <q-list>
-        <q-item
-          v-for="(column, i) in columns"
-          :key="i"
-          @click="getPivotValues(column)"
-          clickable
-          v-close-popup
-        >
-          <q-item-section>
-            <q-item-label>{{ column }}</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-btn-dropdown>
+    <div class="text-h6 text-weight-light">
+      Pick one column out of
+      <q-btn-dropdown
+        :disable="!columnsReady"
+        class="bg-transparent text-green"
+        dense
+        label="Available Columns"
+      >
+        <q-list>
+          <q-item
+            v-for="(column, i) in columns"
+            :key="i"
+            @click="getPivotValues(column); showAnalyzerHeader=true"
+            clickable
+            v-close-popup
+          >
+            <q-item-section>
+              <q-item-label>{{ column }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+      to analyze
+    </div>
+
+    <!-- The multi-graph section -->
+    <div v-if="showAnalyzerHeader" class="q-my-md q-pa-sm bg-dark">
+      <h3 class="text-weight-light q-pa-sm text-overline" style="font-size: 150%;">
+        Analyzing column <code class="bg-green-9 q-pa-xs">{{ activecolumn }}</code>
+      </h3>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  components: {
+    // Graph: () => import(/*webpackChunkName: Graph*/'components/Graph')
+  },
+
   data() {
     return {
       // hold names of all columns as an array
@@ -27,6 +47,12 @@ export default {
 
       // do not activate the dropdown until the columns are done fetching
       columnsReady: false,
+
+      // show analyzer section heading only when a value is selected
+      showAnalyzerHeader: false,
+
+      // the column to pivot against (aka the active column)
+      activecolumn: null,
     }
   },
 
@@ -45,6 +71,7 @@ export default {
     },
 
     getPivotValues(column) {
+      this.activecolumn = column
       const endpoint = process.env.DEV_API + '/pivot?about=' + column
       this.$axios
         .get(endpoint)
